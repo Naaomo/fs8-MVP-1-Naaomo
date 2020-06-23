35ab769250af6481fb45df8ab74723601563aa36
naaomo <cindywongjx@gmail.com>
2020-06-23 18:42:27 +0800

    Intial commit

---
+++ react/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  render(){
+   return (
+    <div className="App">
+      <header className="App-header">Testerino</header>
+    </div>
+   );
+  }
+}
+
+export default App;
